d82f6966f9a968aee37e7b4cd1f6763cddbee5d5
baesm971013 <bobbyb537@naver.com>
2024-01-22 19:56:12 +0900

    add Test Code

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -11,6 +11,22 @@ public Member(Long id, String name, Grade grade){
         this.grade = grade;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+
     public Long getId() {
         return id;
     }
