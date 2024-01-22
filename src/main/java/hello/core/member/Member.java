2ff45611472b49058fa5432d84ff2debe1c07e21
baesm971013 <bobbyb537@naver.com>
2024-01-22 19:31:09 +0900

    feat: start purchase web service

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,11 @@
+package hello.core.member;
+
+public class Member {
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id) {
+        this.id = id;
+    }
+}
