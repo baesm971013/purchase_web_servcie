badd76a0aee7674eb0561b514cba233626da419a
baesm971013 <bobbyb537@naver.com>
2024-01-24 19:45:53 +0900

    add spring bean

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,20 +7,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import hello.core.order.Order;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService
+                = applicationContext.getBean("memberService",MemberService.class);
+        OrderService orderService
+                = applicationContext.getBean("orderService",OrderService.class);
 
         Long memberId = 1L;
-        Member member = new Member(memberId, "memberA", Grade.VIP);
+        Member member = new Member(memberId, "basesm", Grade.BASIC);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA",20000);
+        Order order = orderService.createOrder(memberId, "hhkbneyboard",30000);
         System.out.println(order);
     }
 }
