e74a70640c67f55016e5bd24cee69ec5d59da050
baesm971013 <bobbyb537@naver.com>
2024-01-28 18:37:20 +0900

    feat: Spring container 사용

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,9 @@ import hello.core.member.MemberService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class SingletonTest {
     @Test
@@ -26,8 +29,17 @@ public class SingletonTest {
         //assertThat(s1).isNotSameAs(s2);
         assertThat(s1).isSameAs(s2);
         //same ==
-        //equal
-
+        //equal Spring Container는 지가 알아서 해줌
+    }
+    @Test
+    @DisplayName("스프링컨테이너와 싱글톤")
+    void springContainer(){
+        ApplicationContext ac
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig appconfig = new AppConfig();
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isSameAs(memberService2);
 
     }
 }
