c35c1177620ab0eb254adbc28e2aa18dc468565a
baesm971013 <bobbyb537@naver.com>
2024-01-28 13:09:17 +0900

    feat: add xml bean 등록 방법

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -31,7 +31,7 @@ public class ApplicationContextExtendsFindTest {
     @DisplayName("부토타입으로 조회시 자식이 둘 이상있으면, 빈 이름을 지정하면 된다")
     void findBeanByParentTypeBean(){
        DiscountPolicy rate =  ac.getBean("rateDiscountPolicy",DiscountPolicy.class);
-       assertThat(rate).isInstanceOf(RateDiscountPolicy.class);
+        assertThat(rate).isInstanceOf(RateDiscountPolicy.class);
     }
 
     @Test
@@ -39,15 +39,22 @@ public class ApplicationContextExtendsFindTest {
     void findBeanBySubType(){
         RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
         assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+
+        FixDiscountPolicy bean2 = ac.getBean(FixDiscountPolicy.class);
+        assertThat(bean2).isInstanceOf(FixDiscountPolicy.class);
+
+
     }
 
     @Test
     @DisplayName("부모 타입으로 모두 조회하기")
     void findAllBeanByParentType(){
         Map<String,DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()){
+            System.out.println(key+beansOfType.get(key));
+        }
     }
-
-
 
     @Configuration
     static class TestConfig{
