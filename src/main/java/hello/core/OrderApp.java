e94995df1e64fa8493dd2f0633529174c055e960
baesm971013 <bobbyb537@naver.com>
2024-02-17 12:15:57 +0900

    reactor AppConfig file

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -10,17 +10,18 @@ import hello.core.order.Order;
 
 public class OrderApp {
     public static void main(String[] args) {
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
-//
-//        long memberId = 1L;
-//        Member member = new Member(memberId, "memberA", Grade.BASIC);
-//        memberService.join(member);
-//
-//        Order order = orderService.createOrder(
-//                memberId,"itemA", 10000
-//        );
-//        System.out.println(order);
+        AppConfig appConfig  = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA",Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(
+                memberId, "itemA", 10000
+        );
+        System.out.println(order);
 
     }
 }
